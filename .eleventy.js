d7d97a295513ccb960900c094b2af9605d7da297
chulminh <60227860+chulminh@users.noreply.github.com>
2021-01-26 19:10:37 -0600

    added jpg to templateformat so images gets outputted into dist

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,20 @@
 // docs: https://www.11ty.io/docs/config/
 
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addWatchTarget("./src/css/");
-    eleventyConfig.setTemplateFormats(["css", "html", "liquid"]);
-  
-    function sortByPageOrder(values) {
-      return values.slice().sort((a, b) => a.data.order - b.data.order);
-    }
-    eleventyConfig.addFilter("sortByPageOrder", sortByPageOrder);
-  
-    return {
-      dir: {
-        includes: "_includes",
-        layouts: "_layouts",
-        input: "src",
-        output: "dist"
-      }
-    };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.setTemplateFormats(["css", "html", "liquid", "jpg"]);
+
+  function sortByPageOrder(values) {
+    return values.slice().sort((a, b) => a.data.order - b.data.order);
+  }
+  eleventyConfig.addFilter("sortByPageOrder", sortByPageOrder);
+
+  return {
+    dir: {
+      includes: "_includes",
+      layouts: "_layouts",
+      input: "src",
+      output: "dist",
+    },
   };
+};
