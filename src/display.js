e7b77b011ba886555909d41349a941e4a7cdebd0
chulminh <60227860+chulminh@users.noreply.github.com>
2021-01-28 01:57:53 -0600

    test w artist and cat name

--- src/display.js
+++ src/display.js
@@ -1,23 +1,17 @@
 var detail = document.getElementById("detail");
 var container = document.querySelector("main");
-//var bigpicture = document.getElementById("detailImage").src;
 
 function panel(event) {
-  detail.style.display = "block";
-  console.log("block");
+  detail.style.width = "30%";
 
   container.style.marginRight = "30%";
 
   var picture = event.target.src;
-  console.log(picture);
 
   document.getElementById("detailImage").src = picture;
 }
 
 function exbutton() {
-  detail.style.display = "none";
-  console.log("none");
+  detail.style.width = "0";
   container.style.marginRight = "0px";
 }
-
-console.log(container.style.marginRight);
